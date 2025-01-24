df1a3e84282adaa2ee7b3d6101fd16e15c22a866
Lucas Caio <lucas.caio777@gmail.com>
2025-01-25 00:48:14 +0100

    begin code unification

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,5 +1,5 @@
-import sys
-import os
+import os, sys
+import pandas as pd
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,2 +1,2 @@
 from .data_loading import load_data, save_data
-from .data_preprocessing import check_missing_values, handle_missing_values, encode_categorical, split_data
+from .data_preprocessing import check_missing_values, handle_missing_values

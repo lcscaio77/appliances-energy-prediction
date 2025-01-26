de84b8336a9d3e916ab6de1e360a1ae7edc0ab24
Lucas Caio <lucas.caio777@gmail.com>
2025-01-26 22:13:21 +0100

    elastic-sarimax model cleaning code 70%

--- notebooks/config.py
+++ notebooks/config.py
@@ -6,11 +6,3 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 RAW_DATA_FILE = '../data/raw/energydata_complete.csv'
 RENAMED_DATA_FILE = '../data/processed/renamed_data.csv'
 PROCESSED_DATA_FILE = '../data/processed/processed_data.csv'
-
-_processed_data = pd.read_csv('../appliances-energy-prediction/data/processed/processed_data.csv')
-
-FEATURES = _processed_data.columns.drop('Energy')
-TARGET = 'Energy'
-
-X = _processed_data[FEATURES].values
-Y = _processed_data['Energy'].values
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,1 @@
-from .model_evaluation import evaluate_model
 from .model_saving import save_model, load_model
